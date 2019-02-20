051400551990a8fcc922cec28c2c3adb9bb21566
schmelto <tom.schmelzer@web.de>
2019-02-20 14:44:00 +0100

    commit

---
+++ html/basicsiteelements/header.php
@@ -0,0 +1,70 @@
+
+    <!-- For dark, when sidebar is open -->
+    <div class="overlay"></div>
+    <!-- Sidebar  -->
+    <nav id="sidebar">
+        <div id="dismiss">
+            <i class="fas fa-times"></i>
+        </div>
+
+        <div class="sidebar-header text-center">
+            <h3>Menü</h3>
+        </div>
+
+        <ul class="list-unstyled components text-center">
+            <li>
+                <a href="#">Login</a>
+            </li>
+            <li>
+                <a href="#">Account</a>
+            </li>
+            <li>
+                <a href="#">Logout</a>
+            </li>
+            <li>
+                <a href="#">Über uns</a>
+            </li>
+            <li>
+                <a href="#">Kontakt</a>
+            </li>
+            <li>
+                <a href="#">Datenschutz</a>
+            </li>
+            <li>
+                <a href="#">Impressum</a>
+            </li>
+        </ul>
+
+        <div id="social">
+            <a href=""><img class="img-fluid" src="images/social_icons/facebook_transparent.PNG"></a>
+            <a href=""><img class="img-fluid" src="images/social_icons/insta_transparent.PNG"></a>
+            <a href=""><img class="img-fluid" src="images/social_icons/twitter_transparent.PNG"></a>
+            <br><br>
+        </div>
+    </nav>
+
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+
+        <div class="container">
+            <!-- Hamburger menue -->
+            <div id="menu">
+                <button id="sidebarCollapse" class="btn">
+                    <i class="fas fa-bars" id="hamburger"></i>
+                </button>
+            </div>
+
+            <div id="placeholder"></div>
+            <!-- Logo -->
+            <a href="#"><img src=" images/Logo_transparent.png" id="img_logo" class="img-fluid" alt="Logo"></a>
+            <div id="placeholder"></div>
+            <!-- Search Bar -->
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="Suchbegriff eingeben">
+                <div class="input-group-append">
+                    <button class="btn btn-secondary" type="button">
+                        <i class="fa fa-search"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </nav>
---
+++ html/index_neu.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1 maximum-scale=1, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>Sharey</title>
+
+    <!-- Bootstrap CSS CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
+        integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <!-- Our Custom CSS -->
+    <link rel="stylesheet" href="css/style3.css">
+    <!-- Scrollbar Custom CSS -->
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
+
+    <!-- Font Awesome JS -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js"
+        integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ"
+        crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js"
+        integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY"
+        crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+
+    <?php
+        include('basicsiteelements/header.php');
+    ?>
+
+    <div class="content">
+
+        <!-- Content -->
+        <div class="container">
+            <div class="row">
+
+                <!-- Card 1 -->
+                <div class="col-sm-4 py-2">
+                    <div class="card h-100 text-white bg-primary" style="width: 21rem;">
+                        <div class="container.fluid">
+                            <div class="row">
+
+                                <!-- Card 1 - Row 1 - Column 1 -->
+                                <div class="col-sm-6">
+                                    <div class="card-body">
+                                        <p class="card-text">Tag</p> <!-- dynamic -->
+                                        <br>
+                                        <img src="images/location.png" style="width: 40px;" style="height: 40px;"
+                                            style="display: inline">
+                                        <p class="card-text" style="display: inline">Mosbach</p> <!-- dynamic -->
+                                    </div>
+                                </div>
+
+                                <!-- Card 1 - Row 1 - Column 2 -->
+                                <div class="col-sm-6">
+                                    <div class="card-body">
+                                        <img src="images/yoghurt.jpg" style="width: 100%;" style="height: 100%">
+                                        <!-- dynamic -->
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!-- Card 1 - Row 2 -->
+                            <div class="row">
+                                <div class="card-body">
+                                    <h5 class="card-title">Ein Joghurt</h5> <!-- dynamic -->
+                                    <p class="card-text">Joghurt von Ehrmann, Geschmacksrichtung Erdbeere</p>
+                                    <!-- dynamic -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Card 2 -->
+                <div class="col-sm-4 py-2">
+                    <div class="card h-100 text-white bg-primary" style="width: 21rem;">
+                        <div class="container.fluid">
+                            <div class="row">
+
+                                <!-- Card 2 - Row 1 - Column 1 -->
+                                <div class="col-sm-6">
+                                    <div class="card-body">
+                                        <p class="card-text">Tag</p>
+                                        <br>
+                                        <img src="images/location.png" style="width: 40px;" style="height: 40px;"
+                                            style="display: inline">
+                                        <p class="card-text" style="display: inline">Mosbach</p>
+                                    </div>
+                                </div>
+
+                                <!-- Card 2 - Row 1 - Column 2 -->
+                                <div class="col-sm-6">
+                                    <div class="card-body">
+                                        <img src="images/yoghurt.jpg" style="width: 100%;" style="height: 100%">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!-- Card 2 - Row 2 -->
+                            <div class="row">
+                                <div class="card-body">
+                                    <h5 class="card-title">Ein Joghurt</h5>
+                                    <p class="card-text">Joghurt von Ehrmann, Geschmacksrichtung Erdbeere</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Card 3 -->
+                <div class="col-sm-4 py-2">
+                    <div class="card h-100 text-white bg-primary" style="width: 21rem;">
+                        <div class="container.fluid">
+                            <div class="row">
+
+                                <!-- Card 3 - Row 1 - Column 1 -->
+                                <div class="col-sm-6">
+                                    <div class="card-body">
+                                        <p class="card-text">Tag</p>
+                                        <br>
+                                        <img src="images/location.png" style="width: 40px;" style="height: 40px;"
+                                            style="display: inline">
+                                        <p class="card-text" style="display: inline">Mosbach</p>
+                                    </div>
+                                </div>
+
+                                <!-- Card 3 - Row 1 - Column 2 -->
+                                <div class="col-sm-6">
+                                    <div class="card-body">
+                                        <img src="images/yoghurt.jpg" style="width: 100%;" style="height: 100%">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!-- Card 3 - Row 2 -->
+                            <div class="row">
+                                <div class="card-body">
+                                    <h5 class="card-title">Ein Joghurt</h5>
+                                    <p class="card-text">Joghurt von Ehrmann, Geschmacksrichtung Erdbeere</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <?php
+            include('basicsiteelements/scripts.php');
+        ?>
+
+</body>
+
+</html>
