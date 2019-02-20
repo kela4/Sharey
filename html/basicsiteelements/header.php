816b452ba926cd022cd1de505379588a43275bbc
schmelto <tom.schmelzer@web.de>
2019-02-20 15:32:27 +0100

    commit

--- html/basicsiteelements/header.php
+++ html/basicsiteelements/header.php
@@ -1,70 +1,22 @@
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1 maximum-scale=1, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <!-- For dark, when sidebar is open -->
-    <div class="overlay"></div>
-    <!-- Sidebar  -->
-    <nav id="sidebar">
-        <div id="dismiss" title="Schließen">
-            <i class="fas fa-times"></i>
-        </div>
+    <title>Sharey</title>
 
-        <div class="sidebar-header text-center">
-            <h3>Menü</h3>
-        </div>
+    <!-- Bootstrap CSS CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
+        integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <!-- Our Custom CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Scrollbar Custom CSS -->
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
 
-        <ul class="list-unstyled components text-center">
-            <li>
-                <a href="#" title="Login">Login</a>
-            </li>
-            <li>
-                <a href="#" title="Account">Account</a>
-            </li>
-            <li>
-                <a href="#" title="Logout">Logout</a>
-            </li>
-            <li>
-                <a href="#" title="Über uns">Über uns</a>
-            </li>
-            <li>
-                <a href="#" title="Kontakt">Kontakt</a>
-            </li>
-            <li>
-                <a href="#" title="Datenschutz">Datenschutz</a>
-            </li>
-            <li>
-                <a href="#" title="Impressum">Impressum</a>
-            </li>
-        </ul>
-
-        <div id="social">
-            <a href="https://www.facebook.com/UmweltministeriumBW/"><img class="img-fluid" src="images/social_icons/facebook_transparent.PNG" alt="Facebook" title="Facebook"></a>
-            <a href="#"><img class="img-fluid" src="images/social_icons/insta_transparent.PNG" alt="Instagram" title="Instagram"></a>
-            <a href="https://twitter.com/UmweltBW"><img class="img-fluid" src="images/social_icons/twitter_transparent.PNG" alt="Twitter" title="Twitter"></a>
-            <br><br>
-        </div>
-    </nav>
-
-    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
-
-        <div class="container">
-            <!-- Hamburger menue -->
-            <div id="menu" title="Menü">
-                <button id="sidebarCollapse" class="btn">
-                    <i class="fas fa-bars" id="hamburger"></i>
-                </button>
-            </div>
-
-            <div id="placeholder"></div>
-            <!-- Logo -->
-            <a href="#"><img src=" images/Logo_transparent.png" id="img_logo" class="img-fluid" alt="Logo" title="Sharey"></a>
-            <div id="placeholder"></div>
-            <!-- Search Bar -->
-            <div class="input-group">
-                <input type="text" class="form-control" placeholder="Suchbegriff eingeben" title="Suchbegriff eingeben">
-                <div class="input-group-append">
-                    <button class="btn btn-secondary" type="button" title="Suche">
-                        <i class="fa fa-search"></i>
-                    </button>
-                </div>
-            </div>
-        </div>
-    </nav>
+    <!-- Font Awesome JS -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js"
+        integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ"
+        crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js"
+        integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY"
+        crossorigin="anonymous"></script>
