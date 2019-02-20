4613353739184e272baade3fe6481356e4df72ed
schmelto <tom.schmelzer@web.de>
2019-02-20 15:35:09 +0100

    commit

--- html/basicsiteelements/navigation.php
+++ html/basicsiteelements/navigation.php
@@ -1,70 +1,65 @@
 
-    <!-- For dark, when sidebar is open -->
-    <div class="overlay"></div>
-    <!-- Sidebar  -->
-    <nav id="sidebar">
-        <div id="dismiss" title="Schließen">
-            <i class="fas fa-times"></i>
+<!-- For dark, when sidebar is open -->
+<div class="overlay"></div>
+<!-- Sidebar  -->
+<nav id="sidebar">
+    <div id="dismiss" title="Schließen">
+        <i class="fas fa-times"></i>
+    </div>
+    <div class="sidebar-header text-center">
+        <h3>Menü</h3>
+    </div>
+
+    <ul class="list-unstyled components text-center">
+        <li>
+            <a href="#" title="Login">Login</a>
+        </li>
+        <li>
+            <a href="#" title="Account">Account</a>
+        </li>
+        <li>
+            <a href="#" title="Logout">Logout</a>
+        </li>
+        <li>
+            <a href="#" title="Über uns">Über uns</a>
+        </li>
+        <li>
+            <a href="#" title="Kontakt">Kontakt</a>
+        </li>
+        <li>
+            <a href="#" title="Datenschutz">Datenschutz</a>
+        </li>
+        <li>
+            <a href="#" title="Impressum">Impressum</a>
+        </li>
+    </ul>
+    <div id="social">
+        <a href="https://www.facebook.com/UmweltministeriumBW/"><img class="img-fluid" src="images/social_icons/facebook_transparent.PNG" alt="Facebook" title="Facebook"></a>
+        <a href="#"><img class="img-fluid" src="images/social_icons/insta_transparent.PNG" alt="Instagram" title="Instagram"></a>
+        <a href="https://twitter.com/UmweltBW"><img class="img-fluid" src="images/social_icons/twitter_transparent.PNG" alt="Twitter" title="Twitter"></a>
+        <br><br>
+    </div>
+</nav>
+<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <!-- Hamburger menue -->
+        <div id="menu" title="Menü">
+            <button id="sidebarCollapse" class="btn">
+                <i class="fas fa-bars" id="hamburger"></i>
+            </button>
         </div>
-
-        <div class="sidebar-header text-center">
-            <h3>Menü</h3>
-        </div>
-
-        <ul class="list-unstyled components text-center">
-            <li>
-                <a href="#" title="Login">Login</a>
-            </li>
-            <li>
-                <a href="#" title="Account">Account</a>
-            </li>
-            <li>
-                <a href="#" title="Logout">Logout</a>
-            </li>
-            <li>
-                <a href="#" title="Über uns">Über uns</a>
-            </li>
-            <li>
-                <a href="#" title="Kontakt">Kontakt</a>
-            </li>
-            <li>
-                <a href="#" title="Datenschutz">Datenschutz</a>
-            </li>
-            <li>
-                <a href="#" title="Impressum">Impressum</a>
-            </li>
-        </ul>
-
-        <div id="social">
-            <a href="https://www.facebook.com/UmweltministeriumBW/"><img class="img-fluid" src="images/social_icons/facebook_transparent.PNG" alt="Facebook" title="Facebook"></a>
-            <a href="#"><img class="img-fluid" src="images/social_icons/insta_transparent.PNG" alt="Instagram" title="Instagram"></a>
-            <a href="https://twitter.com/UmweltBW"><img class="img-fluid" src="images/social_icons/twitter_transparent.PNG" alt="Twitter" title="Twitter"></a>
-            <br><br>
-        </div>
-    </nav>
-
-    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
-
-        <div class="container">
-            <!-- Hamburger menue -->
-            <div id="menu" title="Menü">
-                <button id="sidebarCollapse" class="btn">
-                    <i class="fas fa-bars" id="hamburger"></i>
+        <div id="placeholder"></div>
+        <!-- Logo -->
+        <a href="#"><img src=" images/Logo_transparent.png" id="img_logo" class="img-fluid" alt="Logo" title="Sharey"></a>
+        <div id="placeholder"></div>
+        <!-- Search Bar -->
+        <div class="input-group">
+            <input type="text" class="form-control" placeholder="Suchbegriff eingeben" title="Suchbegriff eingeben">
+            <div class="input-group-append">
+                <button class="btn btn-secondary" type="button" title="Suche">
+                    <i class="fa fa-search"></i>
                 </button>
             </div>
-
-            <div id="placeholder"></div>
-            <!-- Logo -->
-            <a href="#"><img src=" images/Logo_transparent.png" id="img_logo" class="img-fluid" alt="Logo" title="Sharey"></a>
-            <div id="placeholder"></div>
-            <!-- Search Bar -->
-            <div class="input-group">
-                <input type="text" class="form-control" placeholder="Suchbegriff eingeben" title="Suchbegriff eingeben">
-                <div class="input-group-append">
-                    <button class="btn btn-secondary" type="button" title="Suche">
-                        <i class="fa fa-search"></i>
-                    </button>
-                </div>
-            </div>
         </div>
-    </nav>
+    </div>
+</nav>
