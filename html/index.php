ed03fd4f466235f83f25e1033e6d3d90e774b0b3
schmelto <tom.schmelzer@web.de>
2019-02-22 00:40:05 +0100

    commit

--- html/index.php
+++ html/index.php
@@ -9,39 +9,7 @@
 
 <body>
 
-    <!-- Modal Login -->
-    <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Login</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <form method="post" action="php/login.php">
-                        <div class="form-group">
-                            <label for="email">E-Mail</label>
-                            <input type="email" required class="form-control" id="email"
-                                placeholder="max.mustermann@beispiel.de" name="mail">
-                        </div>
-                        <div class="form-group">
-                            <label for="password">Passwort</label>
-                            <input type="password" requeired class="form-control" id="password" placeholder="Passwort"
-                                name="password">
-                        </div>
-                        <!-- Javascript der Login Modal schließt -->
-                        <button type="submit" class="btn btn-dark float-right">Login</button>
-                        <button type="button" class="btn btn-light float-right" data-toggle="modal"
-                            data-target="#registerModal">Registrieren</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!--end Modal-->
+
 
     <!-- Modal Register -->
     <div class="modal fade" id="registerModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
