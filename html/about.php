db81af30689c7c6575885f5ce2afdf873fc6882b
kela4 <kath.ehrmann@web.de>
2019-02-22 05:02:51 +0100

    ke

--- html/about.php
+++ html/about.php
@@ -19,7 +19,7 @@
         <!-- Div content for padding-top (header) -->
         <div class="container">
             <h1>Über uns</h1>
-
+            <br>
             <p>
                 In der heutigen, schnelllebigen, modernen Welt landen jedes Jahr <strong>elf Millionen Tonnen</strong>
                 Lebensmittel in Deutschland im Müll.
@@ -28,12 +28,31 @@
             </p>
             <p>
                 Das große Problem hierbei: sowohl für die Erzeugung, als auch die Vernichtung dieser weggeworfenen
-                Lebensmittel werden wertvolle Ressourcen verschwendet wir z. B. Rohstoffe, Energie und Wasser. <br>
+                Lebensmittel werden wertvolle Ressourcen verschwendet wie z. B. Rohstoffe, Energie und Wasser. <br>
             </p>
             <p>
                 Das lässt sich nicht nur auf Lebensmittel eingrenzen. Auch beispielsweise Drogerie- oder sonstige
                 Verbrauchsprodukte fallen hier ins Gewicht.
             </p>
+            <br>
+            <center>
+                <h4>
+                    An dieser Stelle kommen wir ins Spiel - <strong>Sharey</strong>
+                </h4>
+            </center>
+            <br>
+            <p>
+                Auf dieser Plattform kannst du dich gegen diese Verschwendung von Ressourcen auf unserer Erde einsetzen.
+            </p>
+            <p>
+                Nach dem Motto <i>bevor du etwas wegschmeißt, das noch nutzbar ist - share it</i> 
+                kannst du dich auf Sharey austoben und einer breiten Nutzergruppe deine nicht mehr benötigten Lebensmittel 
+                und sonstige Verbrauchsprodukte bereitstellen. Gib diesen die Chance, dass sie doch noch genutzt werden!
+            </p>
+            <p>
+                Wir freuen uns auf ein fröhliches Sharing mit euch - euer Sharey-Team.
+            </p>
+            <small>Made with <span>&#10084;</span> by FoStDev. Für Umweltschutz und .<small>
         </div>
     </div>
     <?php
