d6fd7b519bf04f84f20b0c68696cef6bc33aa584
kela4 <kath.ehrmann@web.de>
2019-02-23 00:56:49 +0100

    ke

--- html/basicsiteelements/scripts.php
+++ html/basicsiteelements/scripts.php
@@ -1,6 +1,14 @@
-<!-- jQuery CDN -->
-<script src="https://code.jquery.com/jquery-3.3.1.min.js"
-    crossorigin="anonymous"></script>
+<!-- jQuery CDN - Slim version (=without AJAX) -->
+<!--<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    crossorigin="anonymous"></script>-->
+
+<!--jQuery CDN-->
+<script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+
 <!-- Popper.JS -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"
     integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ"
