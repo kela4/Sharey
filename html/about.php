40c282f29f9c465135bdc5291e812d67b0e29951
kela4 <kath.ehrmann@web.de>
2019-02-22 04:38:57 +0100

    ke

--- html/about.php
+++ html/about.php
@@ -19,6 +19,15 @@
         <!-- Div content for padding-top (header) -->
         <div class="container">
             <h1>Über uns</h1>
+
+            <p>
+                In der heutigen, schnelllebigen, modernen Welt landen jedes Jahr <strong>elf Millionen Tonnen</strong> Lebensmittel in Deutschland im Müll. 
+                Diese Verschwendung wird nicht nur von Großverbrauchern und im Handel verursacht, auch die Privathaushalte haben einen nicht unerheblichen Anteil daran. <br>
+
+                Das große Problem hierbei: sowohl für die Erzeugung, als auch die Vernichtung dieser weggeworfenen Lebensmittel werden wertvolle Ressourcen verschwendet wir z. B. Rohstoffe, Energie und Wasser. <br>
+                
+                Das lässt sich nicht nur auf Lebensmittel eingrenzen. Auch beispielsweise Drogerie- oder sonstige Verbrauchsprodukte fallen hier ins Gewicht.
+            </p>
         </div>
     </div>
     <?php
