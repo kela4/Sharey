f5268802d84ff18acefbae2a5a87ef7fbd6e6bbf
kela4 <kath.ehrmann@web.de>
2019-02-23 14:02:56 +0100

    ke

--- html/php/login.php
+++ html/php/login.php
@@ -1,6 +1,11 @@
 <?php 
 
     require_once('classes/User.php');
+    require_once('classes/PLZ.php');
+    require_once('classes/Tag.php');
+    require_once('classes/Offer.php');
+    require_once('classes/Message.php');
+    require_once('classes/Conversation.php');
     require_once('dbconnect.php');
 
     if(isset($_POST['mail']) && !empty($_POST['mail'])){
