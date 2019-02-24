5a7c8df10c35a89bbd36c1c5a3591c530c04d5fc
gramoerpsel <github@gramoerpsel.de>
2019-02-24 13:35:34 +0100

    Update howto.php

--- html/howto.php
+++ html/howto.php
@@ -18,8 +18,29 @@
     <div class="content">
         <!-- Div content for padding-top (header) -->
         <div class="container">
+            <h1>How To</h1>
+
+			<h3>Eine kurze Anleitung, wie du mitmachen kannst!</h3>
+
+                <div class="col-12 col-md-5 text-center">
+                    <img src="images/1.png" class="img-fluid rounded" alt="1" />
+                </div>
+
+                <div class="col-12 col-md-7">
+
+                    <p>
+                        Auf dieser Plattform kannst du dazu beitragen, die Verschwendung von Ressourcen auf unserer Erde
+                        zu reduzieren. Hier erklären wir dir Schritt für Schritt, wie du ein Angebot anlegen kannst und es übergibst.
+                    </p>
             
-            <!--hier bitte deinen Inhalt einfügen :)-->
+                    <p>
+                        Um ein Angebot anzulegen, klickst du auf unserer Startseite auf den runden + Button in der unteren Bildschirmmitte.
+						Falls du noch nicht eingeloggt bist, erhälst du nun die Möglichkeit, dich zu registrieren oder einzuloggen.
+                    </p>
+
+                </div>
+
+            </div>
 
         </div>
     </div>
@@ -27,5 +48,4 @@
         include('basicsiteelements/scripts.php');
     ?>
 </body>
-
 </html>
