32cdc9eb93c5c5a5bc75e25b987a7ab53888943d
kela4 <kath.ehrmann@web.de>
2019-02-25 10:34:40 +0100

    ke

--- html/php/getOffer.php
+++ html/php/getOffer.php
@@ -4,11 +4,11 @@ require_once('classes/Tag.php');
 require_once('classes/Offer.php');
 
 if(isset($_POST['offerID']) && !empty($_POST['offerID'])){
-    $offer = Offer::getOffer(intval($_POST['offerID']));
+    //$offer = Offer::getOffer(intval($_POST['offerID']));
 
     if(!empty($offer)){
         $return = json_encode(array(
-            'offerAvailable' => true,
+            //'offerAvailable' => true,
             'offer' => $offer->toJson()));
     }else{
         $return = json_encode(array(
