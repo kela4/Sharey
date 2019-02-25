206193e10d9b8658a1ea19e23bb27e2f00f89f6e
schmelto <tom.schmelzer@web.de>
2019-02-26 00:28:47 +0100

    commit

--- html/modal/modalLogin.php
+++ html/modal/modalLogin.php
@@ -11,7 +11,7 @@
             </div>
             <div class="modal-body">
                 <form method="post" action="../php/login.php">
-                    <div class="form-group" tabindex="1">
+                    <div class="form-group" tabindex="0">
                         <label for="email">E-Mail</label>
                         <input type="email" required class="form-control" id="email"
                             placeholder="max.mustermann@beispiel.de" name="mail">
