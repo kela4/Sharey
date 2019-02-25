aecd2cf0454c66ca8ff89d4df15f882c91e71e44
kela4 <kath.ehrmann@web.de>
2019-02-25 12:33:16 +0100

    ke

--- html/php/isLoggedInJSON.php
+++ html/php/isLoggedInJSON.php
@@ -1,6 +1,7 @@
 <?php 
     require_once('classes/User.php');
         
+    session_start();
     if(isset($_SESSION['user']) && !empty($_SESSION['user'])){
         echo json_encode(array("test" => true, "user" => $_SESSION['user']));
     }else{
