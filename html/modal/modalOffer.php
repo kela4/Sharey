8a42266af0be0d745c1ddf15a5698bbc8e2d64a1
kela4 <kath.ehrmann@web.de>
2019-02-25 11:24:24 +0100

    ke

--- html/modal/modalOffer.php
+++ html/modal/modalOffer.php
@@ -4,7 +4,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header omColor" id="omHeader">
-                <h3 class="modal-title whiteText" id="exampleModalLabel"></h3>
+                <h3 class="modal-title whiteText" id="exampleModalLabel">Angebot</h3>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
