8182384c72803c2da28ba6a41dd3f0a93c9d4b84
kela4 <kath.ehrmann@web.de>
2019-02-26 16:52:28 +0100

    show "ich" before own sended messages in account page

--- html/account.php
+++ html/account.php
@@ -126,6 +126,11 @@
                                                         $messageUnreaded = "font-weight-bold"; //set this class to last message textdiv
                                                 }
 
+                                                $messageSendedByCurrentUser = "";
+                                                if($conversation->getLastMessage()->getSenderID() == $_SESSION['user']->getUserID()){
+                                                    $messageSendedByCurrentUser = "Ich: ";
+                                                }
+
                                                 //if offer-counter ist ungerade, dann dunkelgrau er div, sonst hellgrau
                                                 echo '<div class="card" id="'.$colorOfferGroup.'">
                                                         <h4>'.$conversation->getOfferTitle().'</h4>
@@ -133,7 +138,7 @@
                                                             <input type="text" hidden required name="conID" value="'.$conversation->getConID().'" />
                                                             <div class="col-12">
                                                                 <button class="btn shadow-none" type="submit">
-                                                                    <div class="'.$messageUnreaded.'" style="display: inline;">@'.$acceptorCounter.' '.$conversation->getLastMessage()->getContent().'</div>
+                                                                    <div class="'.$messageUnreaded.'" style="display: inline;">@'.$acceptorCounter.' '.$messageSendedByCurrentUser.$conversation->getLastMessage()->getContent().'</div>
                                                                     <div class="float-right" id="'.$colorTimeStamp.'">'.$conversation->getLastMessage()->getDate()->format('Y-m-d H:i').'</div>
                                                                 </button>
                                                             </div>
