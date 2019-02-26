b5dda91a9e89316030b52da214135e0d10cdad79
schmelto <tom.schmelzer@web.de>
2019-02-26 17:35:25 +0100

    howto

--- html/howto.php
+++ html/howto.php
@@ -23,23 +23,49 @@
 
             <h3>Eine kurze Anleitung, wie du mitmachen kannst!</h3>
 
+            <div class="row">
+                <div class="col-12 col-xl-2 col-lg-2 col-md-12 col-sm-12 text-center">
+                    <img src="images/1.png" class="img-fluid rounded" alt="1" />
+                </div>
+
+                <div class="col-12 col-xl-20 col-lg-10 col-md-12 col-sm-12">
+
+                    <p>
+                        Auf dieser Plattform kannst du dazu beitragen, die Verschwendung von Ressourcen auf unserer Erde
+                        zu reduzieren. Hier erklären wir dir Schritt für Schritt, wie du ein Angebot anlegen kannst und
+                        es
+                        übergibst.
+                    </p>
+
+                    <p>
+                        Um ein Angebot anzulegen, klickst du auf unserer Startseite auf den runden + Button in der
+                        unteren
+                        Bildschirmmitte.
+                        Falls du noch nicht eingeloggt bist, erhälst du nun die Möglichkeit, dich zu registrieren oder
+                        einzuloggen.
+                    </p>
+
+                </div>
+            </div>
+
+            <hr id="divider">
+
             <div id="textfloatleft" class="col-12 col-md-2 text-center">
-                <img src="images/1.png" class="img-fluid rounded" alt="1" />
+                <img src="images/2.png" class="img-fluid rounded" alt="2" />
             </div>
 
             <div class="col-12 col-md-10 float-right">
 
                 <p>
-                    Auf dieser Plattform kannst du dazu beitragen, die Verschwendung von Ressourcen auf unserer Erde
-                    zu reduzieren. Hier erklären wir dir Schritt für Schritt, wie du ein Angebot anlegen kannst und es
-                    übergibst.
+                    Nun siehst du ein leeres Angebot, dass du mit deinen Angaben ausfüllst.
+                    Beschreibe am besten kurz, was du anbieten möchtest und warum. Auch den Zustand sollstest du
+                    beschreiben, als z.B. ob das Produkt ungeöffnet oder geöffnet ist.
                 </p>
 
                 <p>
-                    Um ein Angebot anzulegen, klickst du auf unserer Startseite auf den runden + Button in der unteren
-                    Bildschirmmitte.
-                    Falls du noch nicht eingeloggt bist, erhälst du nun die Möglichkeit, dich zu registrieren oder
-                    einzuloggen.
+                    Wichtig ist auch, dass du das Mindesthaltbarkeitsdatum (MHD) angibst.
+                    Keine Angst, nur weil das MHD abgelaufen ist, werden wir dein Angebot nicht löschen. Denn fast immer
+                    sind Lebensmittel und Drogerieprodukte auch nach dem MHD noch genießbar.
                 </p>
 
             </div>
