476ec33d477839e3e714d8dde53926f9a6a9a6e6
kela4 <kath.ehrmann@web.de>
2019-02-26 17:55:37 +0100

    ke

--- html/conversation.php
+++ html/conversation.php
@@ -64,7 +64,7 @@
 
 
             <div id="conversationHeading">
-                <h1>Konversation zu <?php $conversation->getOfferTitle();?></h1>
+                <h1>Konversation zu <?php echo $conversation->getOfferTitle();?></h1>
             </div>
 
             <div id="messageArea">
