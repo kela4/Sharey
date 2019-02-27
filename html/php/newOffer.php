587483a9240fd46e7021936249bfd25a34ffa385
kela4 <kath.ehrmann@web.de>
2019-02-27 15:35:32 +0100

    ke

--- html/php/newOffer.php
+++ html/php/newOffer.php
@@ -15,14 +15,16 @@
             
             $imageData = null;
 
-            if(isset($_POST['img']) && !empty($_POST['img']) && $_POST['img'] != ""){
+            if(isset($_POST['img']) && !empty($_POST['img']) && ($_POST['img'] != "") && (strpos($_POST['img'], 'data:image') === 0)){
                 $image = $_POST['img'];
+
                 if(strpos($image, 'data:image/jpeg;base64,') === 0){
                     $image = str_replace('data:image/jpeg;base64,', '', $image);
                 }
                 if(strpos($image, 'data:image/png;base64,') === 0){
                     $image = str_replace('data:image/png;base64,', '', $image);
                 }
+                
                 $image = str_replace(' ', '+', $image);
                 $imageData = base64_decode($image);
             }
