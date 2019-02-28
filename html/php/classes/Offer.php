7628541305c86760b70e9881a37e3e4c749d1304
kela4 <kath.ehrmann@web.de>
2019-02-28 20:06:55 +0100

    ke

--- html/php/classes/Offer.php
+++ html/php/classes/Offer.php
@@ -143,8 +143,12 @@ class Offer{
         $startDistanceX = 49.35360; //latitude
         
         while(($data = mysqli_fetch_array($res)) != false){
-            $distanceFromMosbach = getDistanceBetween(floatval($startDistanceX), floatval($startDistanceY), floatval(48.35360), floatval(10.15110));
-            $distanceFromMosbach = round(111*1/$distanceFromMosbach); //in km
+            $distanceFromMosbach = getDistanceBetween(floatval($startDistanceX), floatval($startDistanceY), floatval($data['pz_latitude']), floatval($data['pz_longitude']));
+            if(isset($distanceFromMosbach) && !empty($distanceFromMosbach) && $distanceFromMosbach != 0){
+                $distanceFromMosbach = round(111*1/$distanceFromMosbach); //in km
+            }else{
+                $distanceFromMosbach = 0;
+            }
             
             //$distanceFromMosbach = 10;
             $offersWithDistanceFromPoint[] = [
